98002ee14f0140c773101be16f0d598bd7dc47fc
Charlotte Keitumetse Lekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-06-03 09:25:19 +0200

    meme file updated
    I have updated the meme file with the full code I got from scrimba.

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "./components/Header"
 
-export default function App() {
+
+
+export default function App (){
     return (
         <div>
-            <Header />
+            <Header/>
         </div>
     )
 }
